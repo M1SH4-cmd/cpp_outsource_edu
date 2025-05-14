589b1fc7e8b25a6f32b22358fdaf941b3851ce64
og_michael <misa.belyakov.ru@gmail.com>
2025-05-14 14:09:04 +0700

    smth done

--- Allocators/main.cpp
+++ Allocators/main.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
+#include <vector>
+
+template<class T>
+class allocator
+{
+public:
+    using value_type = T;
+
+    allocator(allocator<int> allocator1) {}
+
+    T* allocate(size_t n)
+    {
+        std::cout << "Items: " << n << " allocate: " << n * sizeof(T) << " bytes\n";
+        return static_cast<T*>(::operator new(n * sizeof(T)));
+    }
+
+    void deallocate(T* ptr, size_t n)
+    {
+        std::cout << "Deallocate: " << n * sizeof(T) << " bytes\n";
+        ::operator delete(ptr);
+    }
+};
 
 int main() {
-    
+    std::vector<int, allocator<int>> vec;
+    vec.reserve(100);
     return 0;
 }
